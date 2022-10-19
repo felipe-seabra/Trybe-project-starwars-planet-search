1a79498f566412470d12af7a170ee955a3c0b7be
Felipe Seabra <fever@pop-os.localdomain>
2022-10-19 17:26:17 -0300

    Criando o fetch para a API

---
+++ src/services/fetchAPI.js
@@ -0,0 +1,10 @@
+const END_POINT = "https://swapi.dev/api/planets";
+
+function fetchAPI() {
+    return fetch(END_POINT)
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => console.log(error));
+};
+
+export default fetchAPI;
